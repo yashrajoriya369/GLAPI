162e2181da74fc6bab088df60febc2a549eff32a
Yash Rajoriya <yashrajoriya369@gmail.com>
2025-02-02 20:46:05 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,54 @@
 import React from 'react';
 import { AiOutlineMail } from "react-icons/ai";
 import { IoEyeOutline } from "react-icons/io5";
-import loginBanner from "../images/banner.jpg"
-
+import { FcGoogle } from "react-icons/fc";
+import { CiTwitter } from "react-icons/ci";
+import loginBanner from "../images/v1043-045a.jpg"
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <>
-            <div className='main-container w-full h-screen bg-zinc-900 text-white'>
+            <div className='main-container w-full h-screen'>
                 <div className='subcontainer'>
-                    <div className='form-container'>
-                        <div className='image-banner'></div>
+                    <div className='form-container flex'>
+                        <div className='image-banner'><img src={loginBanner} alt='image-banner' /></div>
                         <div className='login-form'>
-                            <h2>Login</h2>
-                            <form>
-                                <label for='email'>
-                                    <input className='' type='email' name='email' placeholder='example@email.com' />
-                                    <span className='input-icon'><AiOutlineMail />
-                                    </span>
-                                </label>
-                                <label for='password'>
-                                    <input className='' type='password' name='password' placeholder='*********' />
-                                    <span className='input-icon'><IoEyeOutline /></span>
-                                </label>
-                                <button className='login-btn bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded' type='submit'>Login</button>
-                            </form>
+                            <div className='main-login-form'>
+                                <h2>Login</h2>
+                                <p>Welcome Back!</p>
+                                <form>
+                                    <label htmlFor='email'>
+                                        <input type='email' name='email' placeholder='example@email.com' />
+                                        <span className='input-icon text-zinc-400'><AiOutlineMail />
+                                        </span>
+                                    </label>
+                                    <label htmlFor='password'>
+                                        <input type='password' name='password' placeholder='Password' />
+                                        <span className='input-icon text-zinc-400 cursor-pointer'><IoEyeOutline /></span>
+                                    </label>
+                                    <div className='forgot-links'>
+                                        <label htmlFor='checkbox'><input className='cursor-pointer' type='checkbox' name='checkbox' />Remember me</label>
+                                        <button className='cursor-pointer'>Reset Password</button>
+                                    </div>
+                                    <button className='login-btn' type='submit'>Sign in</button>
+                                </form>
+                                <div className='other-links'>
+                                    <div className='first-level'>
+                                        <p>Don't have an account?</p>
+                                        <button>Sign up</button>
+                                    </div>
+                                    <div className='second-level'>
+                                        <hr /><p>or</p><hr />
+                                    </div>
+                                    <div className='third-level'>
+                                        <button className='flex justify-center items-center cursor-pointer'><FcGoogle /><p>Continue with Google</p></button>
+                                        <button className='flex justify-center items-center cursor-pointer'><CiTwitter /><p>Continue with Twitter</p></button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className='image-banner'><img src={loginBanner} alt='image-banner' /></div>
                 </div>
             </div>
         </>
